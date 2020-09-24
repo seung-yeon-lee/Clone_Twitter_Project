f42fe648017c92ffaac1163d30492f412a8dcaa2
seung-yeon-lee <54011561+seung-yeon-lee@users.noreply.github.com>
2020-09-24 22:15:50 +0900

    Add Firebase_instance

---
+++ src/firebase_instance.js
@@ -0,0 +1,21 @@
+import * as firebase from "firebase/app";
+import "firebase/auth";
+import "firebase/firestore";
+import "firebase/storage";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMATIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGIN_ID,
+  appId: process.env.REACT_APP_APP_ID,
+};
+
+firebase.initializeApp(firebaseConfig);
+
+export const firebaseInstance = firebase;
+export const authService = firebase.auth(); // Authentication setting 인증관리용
+export const db = firebase.firestore(); // cloud Firestore setting 데이터베이스용
+export const storageService = firebase.storage(); // storage setting 파일업로드용
